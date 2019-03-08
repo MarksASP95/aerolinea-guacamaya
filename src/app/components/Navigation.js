a16a6c43cd74e116c79dc4e228dac8d11d4a0909
Marco <marcosuarezpetrillo@hotmail.com>
2019-03-08 15:01:23 -0400

    switched to invisible for dev purp

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter, Link, Route, NavLink } from 'react-router-dom';
-import Home from './Home';
+import Home from './Home/Home';
 import About from './About';
 
 class Navigation extends Component {
@@ -15,7 +15,7 @@ class Navigation extends Component {
 
     render(){
         return(
-            <div>
+            <div className="inactive">
                 <NavLink to="/home">Home</NavLink>
                 <NavLink to="/about">About</NavLink>
             </div>
