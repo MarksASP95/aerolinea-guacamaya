0e4f9c1a85167efac5692c777aa2b1d368872cda
Marco <marcosuarezpetrillo@hotmail.com>
2019-03-11 14:53:27 -0400

    testing with local db (maduro co*o e tu madre)

--- src/database.js
+++ src/database.js
@@ -2,8 +2,21 @@ const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
-const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'uvyfncr6ykonmf1f', 'iwZLnOpfJzJkhNtwfFgV', {
+/* const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'uvyfncr6ykonmf1f', 'iwZLnOpfJzJkhNtwfFgV', {
     host: 'bwcu5y15a62g3sdbkekx-mysql.services.clever-cloud.com',
+    dialect: 'mysql',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}); */
+
+const sequelize = new Sequelize('guacamaya', 'root', 'ronaldo', {
+    host: 'localhost',
     dialect: 'mysql',
     operatorsAliases: false,
 
