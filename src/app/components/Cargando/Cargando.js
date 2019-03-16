1b7dda11e8720ad4592fe556433a07e0bafd53d6
Marco <marcosuarezpetrillo@hotmail.com>
2019-03-15 22:23:28 -0400

    Added loading screen component

---
+++ src/app/components/Cargando/Cargando.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import './Cargando.css'
+
+class Cargando extends Component {
+
+
+    render(){
+
+
+        return(
+
+            <div id="loading-component">
+                <div id="loading">
+                    <p>Cargando opciones</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Cargando;
