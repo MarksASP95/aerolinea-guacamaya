3ac3e1ff46594b0b40d17e89aa02088d1bc23552
Marco <marcosuarezpetrillo@hotmail.com>
2019-03-16 13:41:38 -0400

    Testing with heroku

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
-/* const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'uvyfncr6ykonmf1f', 'iwZLnOpfJzJkhNtwfFgV', {
+const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'uvyfncr6ykonmf1f', 'iwZLnOpfJzJkhNtwfFgV', {
     host: 'bwcu5y15a62g3sdbkekx-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     operatorsAliases: false,
@@ -14,8 +14,8 @@ const path = require('path');
         idle: 10000
     }
 });
- */
-const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'root', 'ronaldo', {
+
+/* const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'root', 'ronaldo', {
     host: 'localhost',
     dialect: 'mysql',
     operatorsAliases: false,
@@ -26,7 +26,7 @@ const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'root', 'ronaldo', {
         acquire: 30000,
         idle: 10000
     }
-});
+}); */
 
 sequelize
     .authenticate()
