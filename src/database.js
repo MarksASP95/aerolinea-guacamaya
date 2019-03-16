a17a3fc8512571f71d2426e5ed76a807f172128d
Marco <marcosuarezpetrillo@hotmail.com>
2019-03-15 22:20:33 -0400

    now using dev db

--- src/database.js
+++ src/database.js
@@ -2,31 +2,31 @@ const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
-const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'uvyfncr6ykonmf1f', 'iwZLnOpfJzJkhNtwfFgV', {
+/* const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'uvyfncr6ykonmf1f', 'iwZLnOpfJzJkhNtwfFgV', {
     host: 'bwcu5y15a62g3sdbkekx-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     operatorsAliases: false,
 
     pool: {
-        max: 5,
+        max: 10,
         min: 0,
         acquire: 30000,
         idle: 10000
     }
 });
-
-/* const sequelize = new Sequelize('guacamaya', 'root', 'ronaldo', {
+ */
+const sequelize = new Sequelize('bwcu5y15a62g3sdbkekx', 'root', 'ronaldo', {
     host: 'localhost',
     dialect: 'mysql',
     operatorsAliases: false,
 
     pool: {
-        max: 5,
+        max: 10,
         min: 0,
         acquire: 30000,
         idle: 10000
     }
-}); */
+});
 
 sequelize
     .authenticate()
