063a85fb745a8f6b2e045641aa8d06007598a6ad
Marco <marcosuarezpetrillo@hotmail.com>
2019-03-15 22:25:39 -0400

    Added HomeButton component

--- src/app/components/Home/HomeButton.js
+++ src/app/components/Home/HomeButton.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 
+import './HomeButton.css'
+
 
 class HomeButton extends Component{
 
+    constructor(props){
+        super(props);
+    }
+
+    clickLink(){
+        //document.getElementBy
+    }
 
     render(){
         return(
             <div className="home-button">
-                <NavLink className="home-button-name" to={"/" + this.props.where}>{this.props.name}</NavLink>
+                <NavLink className="home-button-name" to={"/" + this.props.where}>
+                <p>{this.props.name}</p>
+                </NavLink>
+                
             </div>
         );
     }
